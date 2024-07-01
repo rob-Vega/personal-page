4f5f75bef98eabaf2d1d0d482a114798a75da180
Roberto Vega <rvegag95@gmail.com>
2024-06-30 23:37:12 -0400

    page updated

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# My Personal Page
+# Personal Page
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-import toggleDarkMode from "./js/utils/toggleDarkMode";
-import ThreeJSScene from "./js/scenes/ThreeJSScene";
+const toggleThemeButton = document.querySelector("#toogle-theme-button");
+const icons = ["☼", "☾"];
 
-new ThreeJSScene();
-toggleDarkMode();
+toggleThemeButton.addEventListener("click", () => {
+  const theme = localStorage.getItem("ui-theme");
+  console.log(typeof theme, theme);
+
+  if (theme === "dark") {
+    toggleThemeButton.innerHTML = icons[1];
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("ui-theme", "light");
+  } else {
+    toggleThemeButton.innerHTML = icons[0];
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("ui-theme", "dark");
+  }
+});
